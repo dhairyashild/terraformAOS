c3961bf815f3dd0fbfa372e9f62d2b6b143ea9b7
DHAIRYASHIL DESAI <dhairyashild300@gmail.com>
2024-01-25 11:59:14 +0530

    vpc modules

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,28 @@
+variable "vpc-cidr"{}
+
+variable "projectname"{}
+
+variable "environment"{}
+
+
+
+
+
+variable "data-private-sub-az2-cidr"{}
+
+variable "data-private-sub-az1-cidr"{}
+
+variable "app-private-sub-az2-cidr"{}
+
+
+
+variable "app-private-sub-az1-cidr"{}
+
+variable "public-sub-az2-cidr"{}
+
+
+variable "public-sub-az1-cidr"{}
+
+
+variable "region"{}
+
